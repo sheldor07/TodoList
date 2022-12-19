e78e21e41aab3da3ed0b9b4cc13424f314d4a28a
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2022-12-19 13:56:42 +0530

    added sort and clear button functionalities

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-let inpNewTask=$("#newTask")
-let addBtn=$("#addBtn")
-let clearBtn=$("#clearBtn")
-let ulTasks=$("#ulTasks")
+let inpNewTask = $("#newTask")
+let addBtn = $("#addBtn")
+let clearBtn = $("#clearBtn")
+let ulTasks = $("#ulTasks")
+let removeBtn=$("#removeBtn")
+let sortBtn=$("#sortBtn")
 
-addBtn.click(()=>{
-    let listItem=$("<li>",
-    {
-        "class": "list-group-item",
-        "text":inpNewTask.val(),
-    }
-    )
-    listItem.click(()=>{
-        listItem.toggleClass('disabled-strikethrough')
-    })
-    ulTasks.prepend(listItem)
-    inpNewTask.val("")
+
+$(inpNewTask).keypress((e) => {
+  if (e.which === 13) {
+    addItem();
+  }
+});
+function addItem() {
+  let listItem = $("<li>", {
+    class: "list-group-item",
+    text: inpNewTask.val(),
+  });
+  listItem.click(() => {
+    listItem.toggleClass("task-done")
+  });
+  ulTasks.prepend(listItem);
+  inpNewTask.val("")
+}
+function clearDone(){
+    $("#ulTasks .task-done").remove()
+}
+function sortDone(){
+
+    ($("#ulTasks .task-done")).appendTo(ulTasks)
+}
+addBtn.click(addItem)
+clearBtn.click(() => {
+  inpNewTask.val("")
 })
-clearBtn.click(()=>{
-
-    inpNewTask.val("")
-
-})
+removeBtn.click(clearDone)
+sortBtn.click(sortDone)
