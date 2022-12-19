440b07efb8c1a4dc3920005a55412c748b721732
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2022-12-19 13:16:57 +0530

    added done not done toggle and enter key functionality

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-$(()=>{
-    let item
+let inpNewTask=$("#newTask")
+let addBtn=$("#addBtn")
+let clearBtn=$("#clearBtn")
+let ulTasks=$("#ulTasks")
+
+addBtn.click(()=>{
+    let listItem=$("<li>",
+    {
+        "class": "list-group-item",
+        "text":inpNewTask.val(),
+    }
+    )
+    listItem.click(()=>{
+        listItem.toggleClass('disabled-strikethrough')
+    })
+    ulTasks.prepend(listItem)
+    inpNewTask.val("")
+})
+clearBtn.click(()=>{
+
+    inpNewTask.val("")
+
 })
